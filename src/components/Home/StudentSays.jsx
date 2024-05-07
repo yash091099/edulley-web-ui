229830118965afa793c3d20c6ac6393267a80b1d
yash jangid <yash@ith.tech>
2024-05-07 08:42:52 +0530

    code merge

--- src/components/Home/StudentSays.jsx
+++ src/components/Home/StudentSays.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import StudentCard from "./StudentCard";
-import video from "../../assets/video-review.svg";
 import profile from "../../assets/ellipse-1@2x.png";
+import video from "../../assets/video-review.svg";
 
 const StudentSays = () => {
   const [isVideoPlaying, setVideoPlaying] = useState(false);
@@ -13,39 +13,38 @@ const StudentSays = () => {
   return (
     <div className="most-searched-countries-container featured-university-container br-b-0 py-5">
       <div className="container">
-        <h1 className="what-we-can-do-title "> What Students say about us</h1>
+        <h1 className="what-we-can-do-title">What Students say about us</h1>
         <p className="what-we-can-do-description">
-          {" "}
-          It is a long established fact that a reader will be distracted by the
-          readable content of a page when looking at its layout. <br></br>
-           The point of
-          using Lorem Ipsum is that it has a more-or-less normal distribution of
-          letters, as opposed to using 'Content here.
+          It is a long established fact that a reader will be distracted by
+          the readable content of a page when looking at its layout. <br />
+          The point of using Lorem Ipsum is that it has a more-or-less normal
+          distribution of letters, as opposed to using 'Content here.
         </p>
       </div>
-      <div className=" my-5 content-container-what-student">
-        <div
-          className="video-container-what-student"
-          // onClick={videoClickHandler}
-        >
-          {!isVideoPlaying && (
+      <div className="my-5 content-container-what-student">
+        <div className="">
+          {isVideoPlaying ? (
+            <div className="">
+              <iframe
+                title="YouTube video"
+                width="100%"
+                height="100%"
+                src="https://www.youtube.com/embed/OLVUrgQ_BbA?autoplay=1&modestbranding=1"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
+            </div>
+          ) : (
             <>
               <img
                 src={video}
                 alt="Video Thumbnail"
                 className="video-thumbnail-what-student"
+                onClick={videoClickHandler}
               />
               <button className="play-button-what-student"></button>
             </>
-          )}
-          {isVideoPlaying && (
-            <iframe
-              className="youtube-video-what-student"
-              src="https://www.youtube.com/embed/a9H_cn0mnhc?autoplay=1"
-              title="YouTube video"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-            ></iframe>
           )}
         </div>
         <div className="reviews-container-what-student">
--- src/components/Home/UniversityHome.jsx
+++ src/components/Home/UniversityHome.jsx
@@ -3,19 +3,44 @@ import { Link } from "react-router-dom";
 import { getUniversities } from "../../Services/dashboard";
 import CustomLoader from "../loader";
 import toast from "react-hot-toast";
-import defaultLogoImage from "../../assets/frame-1686560972@2x.png"; // Default logo image
+import defaultLogoImage from "../../assets/frame-1686560972@2x.png";
+import { makeStyles } from "@mui/styles";
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+
+const useStyles = makeStyles((theme) => ({
+  arrow: {
+    color: "#FF6477",
+    fontSize: "2rem",
+  },
+  carouselContainer: {
+    display: 'flex',
+    overflowX: 'auto',
+    height: '300px',
+    scrollSnapType: 'x mandatory',
+    scrollbarWidth: 'none', // For Firefox
+    '&::-webkit-scrollbar': {
+      display: 'none', // For Chrome, Safari and Opera
+    },
+  },
+  carouselItem: {
+    flex: '0 0 auto',
+    width: '25%', // Adjust this value to display more or fewer items
+    scrollSnapAlign: 'start',
+  },
+}));
 
 const UniversitiesHome = () => {
+  const classes = useStyles();
   const [universities, setUniversities] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   const fetchUniversities = async () => {
     try {
       const res = await getUniversities();
-      console.log(res, '------------------------universities');
+      console.log(res, "------------------------universities");
       if (!res?.data?.error) {
-        // Adjust this according to the actual response structure
-        setUniversities(res.data.data.slice(0, 4)); // Only taking the first 4 universities
+        setUniversities(res.data.data);
       } else {
         toast.error("Failed to load universities data.");
       }
@@ -39,32 +64,52 @@ const UniversitiesHome = () => {
         <h1 className="what-we-can-do-title">Featured Universities</h1>
         <p className="what-we-can-do-description">
           It is a long established fact that a reader will be distracted by the
-          readable content of a page when looking at its layout.
-          <br />
-          The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.
+          readable content of a page when looking at its layout. <br /> The
+          point of using Lorem Ipsum is that it has a more-or-less normal
+          distribution of letters.
         </p>
-        <div className="countries-container gap-1 my-5 mb-5">
+        <div className={classes.carouselContainer}>
           {universities.map((uni, index) => (
-            <Link to="/institution-details" key={index}>
-              <div className="countries uni_card">
-                <img src={uni.bannerImage || defaultLogoImage} alt="University" className="university-image" style={{ height: "200px", width: "100%", objectFit: "contain", background: '#fff' }} />
-                <div className="card-info">
-                  <div>
-                    <h3 className="university-name">{uni?.universityName || "University Name"}</h3>
-                    <h5 className="university-location">{`${uni.city}, ${uni?.country}`}</h5>
-                  </div>
-                  <div className="university-logo" style={{ background: "none" }}>
-                    {/* Ensure the university logo is styled appropriately, without borderRadius if not needed */}
-                    <img src={uni.universityLogo || defaultLogoImage} alt="Logo" style={{ height: "auto", width: "100%", borderRadius: "50%", objectFit: "contain" }} />
+            <div key={index} className={classes.carouselItem}>
+              <Link to="/institution-details">
+                <div className="countries uni_card">
+                  <img
+                    src={uni.bannerImage || defaultLogoImage}
+                    alt="University"
+                    className="university-image"
+                    style={{
+                      height: "200px",
+                      width: "100%",
+                      objectFit: "contain",
+                      background: "#fff",
+                    }}
+                  />
+                  <div className="card-info">
+                    <div>
+                      <h3 className="university-name">
+                        {uni?.universityName || "University Name"}
+                      </h3>
+                      <h5 className="university-location">{`${uni.city}, ${uni?.country}`}</h5>
+                    </div>
+                    <div className="university-logo" style={{ background: "none" }}>
+                      <img
+                        src={uni.universityLogo || defaultLogoImage}
+                        alt="Logo"
+                        style={{
+                          height: "auto",
+                          width: "100%",
+                          borderRadius: "50%",
+                          objectFit: "contain",
+                        }}
+                      />
+                    </div>
                   </div>
                 </div>
-              </div>
-            </Link>
+              </Link>
+            </div>
           ))}
-            {!universities.length && (
-            <p className="text-center">No universities found.</p>
-          )}
         </div>
+        {!universities.length && <p className="text-center">No universities found.</p>}
       </div>
     </div>
   );
