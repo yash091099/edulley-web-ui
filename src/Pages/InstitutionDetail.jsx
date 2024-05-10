c901b37fd18f1d5bc026ba512f4bf2dace71ab33
yash jangid <yash@ith.tech>
2024-05-10 14:47:17 +0530

    merge code

--- src/Pages/InstitutionDetail.jsx
+++ src/Pages/InstitutionDetail.jsx
@@ -42,49 +42,41 @@ const InstitutionDetail = () => {
             <p>{location.state?.universityDetails?.admissionReq}</p>
 
             <h2 className="mt-5 fw-semibold ">University Stats</h2>
-            <div className="d-flex justify-content-center align-items-center gap-3 flex-wrap award_imgs">
+            <div className="d-flex gap-3 flex-wrap award_imgs">
 
               <div className="card text-center d-flex flex-column justify-content-center align-items-center p-3"
-                style={{ width: '236px', height: '256px', borderRadius: '16px', backgroundColor: '#FFF', boxShadow: '0 4px 8px rgba(255, 100, 119, 0.5)' }}>
-                <div className="icon mb-2" style={{ width: '54px', height: '54px', paddingTop: '4.5px' }}>
-                  <FaAward size="100%" color="#FF6477" />
-                </div>
+                style={{ width: '200px', height: '200px', borderRadius: '16px', backgroundColor: '#FFF', boxShadow: '0 4px 8px rgba(255, 100, 119, 0.5)' }}>
+              
                 <div className="number mb-2" style={{ fontSize: '40px', lineHeight: '48px', color: '#FF6477' }}>
                   {location.state?.universityDetails?.universityStats?.studentSatisfactionRate || 0}
                 </div>
                 <div className="title" style={{ fontSize: '18px', lineHeight: '22px', color: '#FF6477' }}>
-                  Student Satisfaction Rate
+                No. of Full-Time Students per Staff
                 </div>
               </div>
               <div className="card text-center d-flex flex-column justify-content-center align-items-center p-3"
-                style={{ width: '236px', height: '256px', borderRadius: '16px', backgroundColor: '#FFF', boxShadow: '0 4px 8px rgba(255, 100, 119, 0.5)' }}>
-                <div className="icon mb-2" style={{ width: '54px', height: '54px', paddingTop: '4.5px' }}>
-                  <FaAward size="100%" color="#FF6477" />
-                </div>
+                style={{ width: '200px', height: '200px', borderRadius: '16px', backgroundColor: '#FFF', boxShadow: '0 4px 8px rgba(255, 100, 119, 0.5)' }}>
+               
                 <div className="number mb-2" style={{ fontSize: '40px', lineHeight: '48px', color: '#FF6477' }}>
-                  {location.state?.universityDetails?.universityStats?.studentsPerStaff || 0}
+                  {location.state?.universityDetails?.universityStats?.studentsPerStaff || 0}+
                 </div>
                 <div className="title" style={{ fontSize: '18px', lineHeight: '22px', color: '#FF6477' }}>
-                  No. of Full-Time Students per Staff
+                  No. of Full-Time Students
                 </div>
               </div>
               <div className="card text-center d-flex flex-column justify-content-center align-items-center p-3"
-                style={{ width: '236px', height: '256px', borderRadius: '16px', backgroundColor: '#FFF', boxShadow: '0 4px 8px rgba(255, 100, 119, 0.5)' }}>
-                <div className="icon mb-2" style={{ width: '54px', height: '54px', paddingTop: '4.5px' }}>
-                  <FaAward size="100%" color="#FF6477" />
-                </div>
+                style={{ width: '200px', height: '200px', borderRadius: '16px', backgroundColor: '#FFF', boxShadow: '0 4px 8px rgba(255, 100, 119, 0.5)' }}>
+               
                 <div className="number mb-2" style={{ fontSize: '40px', lineHeight: '48px', color: '#FF6477' }}>
-                  {location.state?.universityDetails?.universityStats?.internationalStudentPercentage || 0}%
+                  {location.state?.universityDetails?.universityStats?.internationalStudentPercentage || 0}
                 </div>
                 <div className="title" style={{ fontSize: '18px', lineHeight: '22px', color: '#FF6477' }}>
                   Percentage of International Students
                 </div>
               </div>
               <div className="card text-center d-flex flex-column justify-content-center align-items-center p-3"
-                style={{ width: '236px', height: '256px', borderRadius: '16px', backgroundColor: '#FFF', boxShadow: '0 4px 8px rgba(255, 100, 119, 0.5)' }}>
-                <div className="icon mb-2" style={{ width: '54px', height: '54px', paddingTop: '4.5px' }}>
-                  <FaAward size="100%" color="#FF6477" />
-                </div>
+                style={{ width: '200px', height: '200px', borderRadius: '16px', backgroundColor: '#FFF', boxShadow: '0 4px 8px rgba(255, 100, 119, 0.5)' }}>
+               
                 <div className="number mb-2" style={{ fontSize: '40px', lineHeight: '48px', color: '#FF6477' }}>
                   {location.state?.universityDetails?.universityStats?.fulltimeStudents || 0}
                 </div>
@@ -98,7 +90,7 @@ const InstitutionDetail = () => {
             <h2 className="mt-5 fw-semibold  mb-4">
               What's unique about the university?
             </h2>
-            <div className="d-flex justify-content-center align-items-center gap-3 flex-wrap award_imgs">
+            <div className="d-flex  gap-3 flex-wrap award_imgs">
               <img className="shadow p-2" style={{ width: "150px", height: "150px" }} src={location?.state?.universityDetails?.uniqueUniversityInfo?.image1 || award} alt="" />
               <img className="shadow p-2" style={{ width: "150px", height: "150px" }} src={location?.state?.universityDetails?.uniqueUniversityInfo?.image2 || award} alt="" />
               <img className="shadow p-2" style={{ width: "150px", height: "150px" }} src={location?.state?.universityDetails?.uniqueUniversityInfo?.image3 || award} alt="" />
